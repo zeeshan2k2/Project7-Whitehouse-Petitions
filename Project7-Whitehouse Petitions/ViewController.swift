e3c17b552f7fe1fc3461796d4fe633afd97aedf6
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-02-09 23:40:12 +0500

    Initial Commit

---
+++ Project7-Whitehouse Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7-Whitehouse Petitions
+//
+//  Created by Zeeshan Waheed on 09/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
