fe034d17dd8c5cf1618599ca40d0a9326b584653
Zeeshan <100070393+zeeshan2k2@users.noreply.github.com>
2024-02-11 02:59:38 +0500

    swift day 33 commit

---
+++ Project7-Whitehouse Petitions/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  Project7-Whitehouse Petitions
+//
+//  Created by Zeeshan Waheed on 11/02/2024.
+//
+
+import Foundation
+
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
---
+++ Project7-Whitehouse Petitions/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7-Whitehouse Petitions
+//
+//  Created by Zeeshan Waheed on 11/02/2024.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
